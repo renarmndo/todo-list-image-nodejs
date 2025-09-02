56ffa6fc6586d0301fbe522e27f9dfa95869fb21
rendani hutagaol <rendanihutagaol1234@gmail.com>
2025-09-02 12:46:48 +0700

    db url status

--- src/config/config.js
+++ src/config/config.js
@@ -3,15 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+
+const { DB_NAME, DB, USER, DB_PASS, DB_URL } = process.env;
+const sequelize = new Sequelize(DB_URL, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  logging: false,
+});
 
 export default sequelize;
